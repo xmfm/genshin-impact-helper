15629681f084566aa0a52a33d5fd313c0d78c3cc
小猫非猫 <30648725+xmfm@users.noreply.github.com>
2021-03-25 18:24:33 +0800

    Create ding.py

---
+++ ding.py
@@ -0,0 +1,22 @@
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
+import requests
+import json
+
+
+headers={'Content-Type': 'application/json'}
+def send_ding(content, preurl, key):
+    msg = {
+        'msgtype': 'text',
+        'text': {'content': content}
+    }
+    timestamp = round(time.time() * 1000)  #时间戳
+    presign = f'{timestamp}\n{key}'  #处理前的签名
+    hmac_code = hmac.new(key.encode(), presign.encode(), digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote(base64.b64encode(hmac_code))
+    url = preurl + f'&timestamp={timestamp}&sign={sign}'
+    result = requests.post(url, data=json.dumps(msg), headers=headers)
+    logging.info(time.strftime('%Y.%m.%d %H:%M:%S'), result.json().get('errmsg', 'error'))
